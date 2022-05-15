343b5975b18318689c9a42c14e902712c796243c
daqn <daqn_5@abv.bg>
2022-05-15 20:55:00 +0300

    Initial commit

---
+++ Task3/script.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    extractAllElements('div')
+}
+
+let array = [];
+
+function extractAllElements(domElement){
+    let ul = document.getElementsByTagName(domElement)
+
+    getAllTagNames(ul)
+
+    let tagNamesElements =(array.map(x => x.toLowerCase()));
+    console.log(tagNamesElements)
+}
+
+function getAllTagNames(tagName){
+    for (let i = 0; i < tagName.length; i++) {
+        let element = tagName[i];
+        
+        array.push(element.tagName)
+
+        if(element.hasChildNodes()){
+            getAllTagNames(element.children)
+        }
+    }
+
+}
+
